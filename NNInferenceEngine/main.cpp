53c52e41dccbcf2e6f6100d33bc84cb516391f21
Shrey <shrey7shrey@gmail.com>
2025-06-10 12:58:43 +0530

    Initial Commit

---
+++ NNInferenceEngine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  NNInferenceEngine
+//
+//  Created by Shrey Sharma on 10/06/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
